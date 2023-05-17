c744eebc6d4768dd9f40cb5724caf0aefbe3b6cf
rayondemiel <maximehumeau44@gmail.com>
2023-05-17 15:06:20 +0200

    union in run.py and add listIIIF

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import click
 import os
 
 from scr.iiif import ManifestIIIF, ImageIIIF
+from scr.iiif_list import ListIIIF
 from scr.utils import make_out_dirs
 from scr.variables import DEFAULT_OUT_DIR
 
@@ -15,8 +16,12 @@ from scr.variables import DEFAULT_OUT_DIR
 # ref 1 https://github.com/PonteIneptique/iiif-random-downloader/blob/main/cli.py
 # ref 2 https://github.com/YaleDHLab/iiif-downloader/blob/master/iiif_downloader/__init__.py#L16
 
+@click.group()
+def run_collect():
+    pass
 
-@click.command()
+
+@run_collect.command()
 @click.argument("url", type=click.STRING)
 @click.option("-i", "--image", "image", type=bool, default=False, is_flag=True, help="Active image api")
 @click.option("-w", "--width", "width", type=str, default="max", help="Width to resize image")
@@ -30,7 +35,8 @@ from scr.variables import DEFAULT_OUT_DIR
                                                                              Default value is [full], [square] to \
                                                                              determine area where the width\
                                                                             and height are both equal.")
-@click.option("-f", "--format", "format", type=click.Choice(['default', 'jpg', 'tif', 'png', 'gif', 'jp2', 'pdf', 'webp']),
+@click.option("-f", "--format", "format",
+              type=click.Choice(['default', 'jpg', 'tif', 'png', 'gif', 'jp2', 'pdf', 'webp']),
               default="default", help="Select image format.")
 @click.option("-a", "--api", "api", type=float, default=3.0, help="Determine API level to change change configuration")
 @click.option("-d", "--directory", "directory", type=click.Path(exists=True, dir_okay=True, file_okay=False),
@@ -41,7 +47,7 @@ from scr.variables import DEFAULT_OUT_DIR
 @click.option("--random", "random", type=bool, is_flag=True, help="To get randomize images according to the "
                                                                   "number indicated")
 @click.option("-v", "--verbose", "verbose", type=bool, is_flag=True, help="Get more verbosity")
-def run_collect(url, **kwargs):
+def iiif_singular(url, **kwargs):
     """
     Running script to get and download iiif images, metadata and manifests. If you want access to specific image IIIF,
     you must activate the option.
@@ -105,6 +111,62 @@ def run_collect(url, **kwargs):
         manifest.save_image()
 
     print("! Finish !")
+
+
+@run_collect.command()
+@click.argument("file", type=click.STRING)
+def iiif_list(file):
+    """
+
+    :file:
+    :return:
+    """
+    manifest_list = ListIIIF()
+    if file.endswith('.txt'):
+        manifest_list.read_txt(file)
+    elif file.endswith('.csv'):
+        delimiter, header, encoding = ";", "infer", "utf-8"
+        name_column = str(input("What is the name of iiif columns ? "))
+        check = str(input("Do you want to change paremeters by default ? press [Y/N] "))
+        print(f"delimiter : {delimiter}")
+        print(f"header : {header}")
+        print(f"encoding: {encoding}")
+        if check != check.lower():
+            delimiter, header, encoding = input(
+                "In the order [DELEMITER HEADER(int,None,infer) ENCODING] separated by space : ").split()
+        manifest_list.read_csv(file, name_column, delimiter=delimiter, encoding=encoding, header=header)
+        print(manifest_list.list_iiif)
+    else:
+        raise FileExistsError("Sorry, your file need to be in csv or txt format.")
+
+
+@run_collect.command()
+@click.argument("url", type=click.STRING)
+@click.option("-d", "--directory", "directory", type=click.Path(exists=True, dir_okay=True, file_okay=False),
+              default="./",
+              help="Directory where to save the images")
+@click.option("-v", "--verbose", "verbose", type=bool, is_flag=True, help="Get more verbosity")
+def get_list_image(url, **kwargs):
+    """
+
+    :param url:
+    :param kwargs:
+    :return:
+    """
+    # Get path
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    if kwargs['directory'] != "./":
+        current_path = os.path.join(current_path, kwargs['directory'])
+
+    manifest = ManifestIIIF(str(url), path=current_path, verbose=kwargs['verbose'])
+    make_out_dirs(manifest.out_dir)
+
+    manifest.save_list_images()
+    manifest.save_metadata()
+    manifest.save_manifest()
+
+    print("Finish")
+    print(f"""You can find the file at the following path : <{manifest.__print_path__('images')}>""")
 
 
 if __name__ == "__main__":
--- scr/iiif_list.py
+++ scr/iiif_list.py
@@ -1,4 +1,27 @@
-#class ManifestListIIF(ManifestIIIF):
-# def __len__()
-#read_csv()
-# #read_txt()
+import pandas as pd
+
+
+class ListIIIF:
+    list_iiif = []
+    len_iiif = 0
+
+    def __len__(self):
+        return len(self.list_iiif)
+
+    def read_csv(self, file: str, column_name: str, **kwargs):
+        df = pd.read_csv(file, **kwargs)
+        print(df)
+        #column = df[[column_name]]
+        #column = column.dropna()
+        #for item in column.values.tolist():
+        #    self.list_iiif.append(item[0])
+        #    self.len_iiif = len(self.list_iiif)
+
+    def read_txt(self, file):
+        with open(file, mode='r') as f:
+            txt = f.read()
+            self.list_iiif = txt.split('\n')
+            self.len_iiif = len(self.list_iiif)
+
+    def __get_next__(self):
+        return next(iter(self.list_iiif))
