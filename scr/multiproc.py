1c36c3201130d2a8c23e694b0eb0dbf5ccfdbcb4
rayondemiel <maximehumeau44@gmail.com>
2023-06-14 18:51:01 +0200

    update multiproc and async. WORKS

--- scr/multiproc.py
+++ scr/multiproc.py
@@ -1,76 +1,84 @@
-import concurrent.futures
 import asyncio
 import aiohttp
-import threading
 import multiprocessing
+import time
+import psutil
 
 from .iiif import ConfigIIIF, ImageIIIF
+from scr.opt.performance import calculate_performance
 
+class IIIFCollector:
+    def __init__(self):
+        pass
 
-class AsyncIIIF(ImageIIIF):
+    async def process_url(self, session, url):
+        async with session.get(url) as response:
+            # Process the response data asynchronously
+            response_data = await response.json()
+            return response_data  # Return the processed data asynchronously
 
+    async def process_urls(self, session, urls):
+        # Create tasks for each URL
+        tasks = [self.process_url(session, url) for url in urls]
 
-    async def __aenter__(self):
-        self._session = aiohttp.ClientSession()
+        # Wait for all tasks to complete
+        results = await asyncio.gather(*tasks)
 
-    async def __aexit__(self, *err):
-        await self._session.close()
-        self._session = None
+        # Process the results
+        for result in results:
+            ok = result
 
-    async def fetch(self, filename=None):
-
-        url = self._format_url(self.url)
-        if self.verbose:
-            print(url)
-        # get filename
-        if filename is None:
-            self.id_img = self.__get_id__(url.split('/')[-5])
-        else:
-            self.id_img = filename
-        try:
-            async with self._session.get(url) as resp:
-                if 200 <= resp.status < 400:
-                    return await resp.json()
-        except Exception as err:
-            #print(err)
-            pass
+    async def run_async(self, urls):
+        # Create an aiohttp.ClientSession within the context of an async with statement
+        # This ensures the session is properly closed
+        async with aiohttp.ClientSession() as session:
+            await self.process_urls(session, urls)
 
 
 class ParallelizeImage(ConfigIIIF):
 
-    @staticmethod
-    def process_urls(urls: list, path='./'):
-        # Create an event loop
-        print('test')
+    def process_chunk(self, chunk):
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-
-        # Create a task for each URL
-        tasks = [loop.create_task(AsyncIIIF(url, path).fetch()) for url in urls]
-        tasks_save = [loop.create_task(AsyncIIIF(url, path).fetch()) for url in urls]
-
-        # Wait for all tasks to complete
-        loop.run_until_complete(asyncio.gather(*tasks))
-        # Process the results
-        for task in tasks:
-            result = task.result()
-            print(result)
-
-        # Close the event loop
+        collector = IIIFCollector()
+        loop.run_until_complete(collector.run_async(chunk))
         loop.close()
 
     def __get_cpu__(self):
         return max(2, multiprocessing.cpu_count() // 2)
 
     @classmethod
-    def run_image(cls, urls, path, manifest=False):
-        # Use a ThreadPoolExecutor for multithreading
-        with concurrent.futures.ThreadPoolExecutor(max_workers=cls().__get_cpu__()) as executor:
-            if manifest:
-                pass
-            else:
-                # regarder la derniere rep : https://chat.openai.com/c/cf85e960-6efc-4b7e-9dc0-3013d1ebf30a
+    def run_image(cls, urls):
+        start_time = time.time()  # Start time
+        # Determine the number of processes
+        num_processes = cls.__get_cpu__()
+        # Determine number chunk validity cpu count
+        if len(urls) > num_processes:
+            num_processes = len(urls)
+        # Split the URLs among processes
+        url_chunks = [urls[i::num_processes] for i in range(num_processes)]
 
-                #executor.submit(cls.process_urls, urls, path)
-                if cls.verbose:
-                    print(f"Number of threads used : {str(threading.active_count())}")
+        # Create a list to hold the process objects
+        processes = []
+
+        for chunk in url_chunks:
+            # Create a process for each chunk
+            process = multiprocessing.Process(target=cls.process_chunk, args=(chunk,))
+            processes.append(process)
+            process.start()
+
+        # Measure CPU and memory usage during execution
+        cpu_percent = []
+        memory_usage = []
+
+        while any(process.is_alive() for process in processes):
+            cpu_percent.append(psutil.cpu_percent())
+            memory_usage.append(psutil.virtual_memory().percent)
+
+        # Wait for all processes to finish
+        for process in processes:
+            process.join()
+
+        # Determine resource and time consumption
+        end_time = time.time()
+        calculate_performance(start_time, end_time, cpu_percent, memory_usage)
