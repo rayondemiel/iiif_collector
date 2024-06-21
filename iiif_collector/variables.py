d67907185dd6b52d0a59da5c0b6a0cdc556bf853
rayondemiel <maximehumeau44@gmail.com>
2024-06-21 15:01:40 +0200

    #4 - unitest utils modules

--- iiif_collector/variables.py
+++ iiif_collector/variables.py
@@ -17,3 +17,51 @@ ImageList = List[Tuple[Uri, Filename]]
 Label = str
 Value = str
 MetadataList = List[Tuple[Label, Value]]
+
+# TESTS
+TEST_IIIF_MANIFEST = {
+    "@context": "http://iiif.io/api/presentation/2/context.json",
+    "@id": "https://example.org/iiif/book1/manifest",
+    "@type": "sc:Manifest",
+    "label": "Sample Book",
+    "metadata": [
+        {
+            "label": "Author",
+            "value": "John Doe"
+        },
+        {
+            "label": "Published",
+            "value": "2023"
+        }
+    ],
+    "sequences": [
+        {
+            "@id": "https://example.org/iiif/book1/sequence/normal",
+            "@type": "sc:Sequence",
+            "canvases": [
+                {
+                    "@id": "https://example.org/iiif/book1/canvas/p1",
+                    "@type": "sc:Canvas",
+                    "label": "Page 1",
+                    "height": 1800,
+                    "width": 1200,
+                    "images": [
+                        {
+                            "@id": "https://example.org/iiif/book1/annotation/p0001-image",
+                            "@type": "oa:Annotation",
+                            "motivation": "sc:painting",
+                            "resource": {
+                                "@id": "https://example.org/iiif/book1/res/page1/full/full/0/default.jpg",
+                                "@type": "dctypes:Image",
+                                "format": "image/jpeg",
+                                "height": 1800,
+                                "width": 1200
+                            },
+                            "on": "https://example.org/iiif/book1/canvas/p1"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
